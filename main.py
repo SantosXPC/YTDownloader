a86b7c62da5e7b29fda4511765a61e269781b397
SantosXPC <santosxpc@gmail.com>
2023-09-24 14:48:34 +0000

    Header information modification

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-# This code allows the user to input a YouTube video URL and then uses the pytube library
-# to download the video stream with the highest resolution from that URL.
+"""
+Author: SantosXPC
+Date: 24/09/2023
+Description: This code allows the user to input a YouTube video URL and then uses the pytube library to download the video stream with the highest resolution from that URL.
+
+Usage: Run the code, copy and paste the last code of any Youtube video, i.e. https://www.youtube.com/watch?v=J3_UPuNxx74, just copy+paste "J3_UPuNxx74" code.
+
+License: GNU General Public License (GPL)
+
+"""
 
 # Import the pytube library for downloading YouTube videos
 import pytube
